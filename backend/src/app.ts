267b74f08d303e23867e1dd4104389184715912a
dkartal <deniz.kartal@3tsoftwarelabs.com>
2024-12-08 20:01:03 +0100

    introducing cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200
+  })
+);
 
 app.use(express.json());
 app.use("/api", router);
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const dbName = process.env.ENV
-  ? process.env.TEST_DB_NAME
-  : process.env.DEV_DB_NAME;
+const dbName =
+  process.env.ENV === "test"
+    ? process.env.TEST_DB_NAME
+    : process.env.DEV_DB_NAME;
 
 if (
   !process.env.DB_HOST ||
@@ -16,6 +17,13 @@ if (
 ) {
   throw new Error("Database environment variables are not properly set.");
 }
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", dbName);
+console.log("ENV:", process.env.ENV);
 
 export const dbClient = new Pool({
   host: process.env.DB_HOST,
