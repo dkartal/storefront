34870197e9579ceaf681b40bbe990ca35163a81e
dkartal <deniz.kartal@3tsoftwarelabs.com>
2024-12-27 12:29:47 +0100

    Add validation for a valid email on the checkout page.

--- frontend/src/app/components/checkout-form/checkout-form.component.ts
+++ frontend/src/app/components/checkout-form/checkout-form.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CartService } from '../../services/cartService';
 import { CheckoutForm } from '../../models/CheckoutForm';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-checkout-form',
@@ -12,6 +13,7 @@ import { CheckoutForm } from '../../models/CheckoutForm';
 })
 export class CheckoutFormComponent {
   cartService: CartService = inject(CartService);
+  notificationService: NotificationService = inject(NotificationService);
   checkoutForm: CheckoutForm = {
     fullname: '',
     address: '',
@@ -22,7 +24,11 @@ export class CheckoutFormComponent {
   constructor(private router: Router) {}
 
   onSubmit() {
-    this.cartService.clearCart();
-    this.router.navigate(['/confirmation']);
+    if (this.checkoutForm.fullname && this.checkoutForm.email && this.checkoutForm.address && this.checkoutForm.creditCardNum) {
+      this.cartService.clearCart();
+      this.router.navigate(['/confirmation']);
+    } else {
+      this.notificationService.showNotification("Please fill out all required fields correctly!");
+    }
   }
 }
